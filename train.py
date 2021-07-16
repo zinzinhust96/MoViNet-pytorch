1484a1107c96617b9832e996a41d10829c452dfd
Nam Doan Ngoc <giang.doanngoc@arsvietnam.com>
2021-07-16 09:54:56 +0700

    first time training

--- train.py
+++ train.py
@@ -8,33 +8,38 @@ import torch
 import transforms as T
 from models import MoViNet
 from config import _C
+import os
+from tqdm import tqdm
 
 torch.manual_seed(97)
 num_frames = 16 # 16
 clip_steps = 2
 Bs_Train = 16
 Bs_Test = 16
+store_path = './results/theft'
 
-def train_iter(model, optimz, data_load, loss_val):
+if not os.path.isdir(store_path):
+    os.mkdir(store_path)
+
+def train_iter(model, optimz, data_load):
     samples = len(data_load.dataset)
     model.train()
     model.cuda()
     model.clean_activation_buffers()
     optimz.zero_grad()
-    for i, (data,_ , target) in enumerate(data_load):
+    for i, (data,_ , target) in enumerate(tqdm(data_load)):
         out = F.log_softmax(model(data.cuda()), dim=1)
         loss = F.nll_loss(out, target.cuda())
         loss.backward()
         optimz.step()
         optimz.zero_grad()
         model.clean_activation_buffers()
-        if i % 50 == 0:
+        if i % 50 == 0: # TODO: log interval (args)
             print('[' +  '{:5}'.format(i * len(data)) + '/' + '{:5}'.format(samples) +
                   ' (' + '{:3.0f}'.format(100 * i / len(data_load)) + '%)]  Loss: ' +
                   '{:6.4f}'.format(loss.item()))
-            loss_val.append(loss.item())
 
-def evaluate(model, data_load, loss_val):
+def evaluate(model, data_load):
     model.eval()
 
     samples = len(data_load.dataset)
@@ -42,7 +47,7 @@ def evaluate(model, data_load, loss_val):
     tloss = 0
     model.clean_activation_buffers()
     with torch.no_grad():
-        for data, _, target in data_load:
+        for data, _, target in tqdm(data_load):
             output = F.log_softmax(model(data.cuda()), dim=1)
             loss = F.nll_loss(output, target.cuda(), reduction='sum')
             _, pred = torch.max(output, dim=1)
@@ -51,11 +56,12 @@ def evaluate(model, data_load, loss_val):
             csamp += pred.eq(target.cuda()).sum()
             model.clean_activation_buffers()
     aloss = tloss / samples
-    loss_val.append(aloss)
     print('\nAverage test loss: ' + '{:.4f}'.format(aloss) +
           '  Accuracy:' + '{:5}'.format(csamp) + '/' +
           '{:5}'.format(samples) + ' (' +
           '{:4.2f}'.format(100.0 * csamp / samples) + '%)\n')
+
+    return aloss, 100.0 * csamp / samples
 
 
 
@@ -72,30 +78,39 @@ transform_test = transforms.Compose([
                                  T.CenterCrop((172, 172))])
 
 print('processing train')
-hmdb51_train = torchvision.datasets.HMDB51('video_data_1/', 'test_train_splits_1/', num_frames,frame_rate=5,
+hmdb51_train = torchvision.datasets.HMDB51('video_data/', 'test_train_splits/', num_frames,frame_rate=5,
                                                 step_between_clips = clip_steps, fold=1, train=True,
                                                 transform=transform, num_workers=2)
 
 
 print('processing test')
-hmdb51_test = torchvision.datasets.HMDB51('video_data_1/', 'test_train_splits_1/', num_frames,frame_rate=5,
+hmdb51_test = torchvision.datasets.HMDB51('video_data/', 'test_train_splits/', num_frames,frame_rate=5,
                                                 step_between_clips = clip_steps, fold=1, train=False,
                                                 transform=transform_test, num_workers=2)
 train_loader = DataLoader(hmdb51_train, batch_size=Bs_Train, shuffle=True)
 test_loader  = DataLoader(hmdb51_test, batch_size=Bs_Test, shuffle=False)
 
-N_EPOCHS = 1
+N_EPOCHS = 100
 
-model = MoViNet(_C.MODEL.MoViNetA0, 600,causal = True, pretrained = False, tf_like = True )
-start_time = time.time()
+model = MoViNet(_C.MODEL.MoViNetA0, 600,causal = False, pretrained = True, tf_like = True )
+# start_time = time.time()
 
-trloss_val, tsloss_val = [], []
 model.classifier[3] = torch.nn.Conv3d(2048, 51, (1,1,1))
 optimz = optim.Adam(model.parameters(), lr=0.00005)
+
+best_val_acc = 0
 for epoch in range(1, N_EPOCHS + 1):
     print('Epoch:', epoch)
-    train_iter(model, optimz, train_loader, trloss_val)
-    evaluate(model, test_loader, tsloss_val)
+    train_iter(model, optimz, train_loader)
+    val_loss, val_acc = evaluate(model, test_loader)
+    if val_acc > best_val_acc:
+        print('Saving best model ...')
+        torch.save(model.state_dict(),
+                   os.path.join(store_path, 'model_epoch{:04d}_loss{:.04f}_acc{:.02f}.pth'
+                               .format(epoch, val_loss, val_acc)))
+        best_val_acc = val_acc
 
-print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
+# print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
 
+# TODO:
+# parse to args
